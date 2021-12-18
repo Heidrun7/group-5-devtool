cef9c93a52d8c14471d838321cdf3b476a688ad0
Heidrun7 <34030912+Heidrun7@users.noreply.github.com>
2021-12-18 23:44:00 +0000

    Remove commented out code

--- printScript_Mac/printScript.gd
+++ printScript_Mac/printScript.gd
@@ -7,17 +7,6 @@ func _ready():
 
 
 func print_tree_pretty_custom(file: File, node: Node, prefix: String, last: bool) -> void:
-	#write hierarchy tree normal
-	#var new_prefix = " ┖╴" if last else " ┠╴"
-	#file.store_string(prefix + new_prefix + node.get_name() + "\n")
-	#var children = node.get_children()
-	#for i in range(children.size()):
-		#new_prefix = "   " if last else " ┃ "
-		#print_tree_pretty_custom(file, children[i], prefix + new_prefix, i == (children.size() - 1))
-	
-	#write hiearchy tree graphviz
-	#var parent_id = id+1
-		#var id1 = id+1
 	
 	file.store_string("dot.node('" + node.get_name() + "', '" + node.get_name() + "')\n")
 	var children = node.get_children()
